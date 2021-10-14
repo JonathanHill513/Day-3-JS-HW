ffade0ccc0bbc3efa81d693ef16f6c67874429e3
JonathanHill513 <jonnyhill513@gmail.com>
2021-10-14 01:02:36 -0400

    push

---
+++ app.js
@@ -0,0 +1,28 @@
+var a= 1;
+var b= 2;
+var c= 3;
+var d= 4;
+var e= 5;
+var f= 6;
+var g= 7;
+var h= 8;
+var i= 9;
+var j= 10;
+
+console.log(a+j);
+console.log(b+e);
+
+console.log(e-c);
+console.log(d-f);
+
+console.log(e*i);
+console.log(f*a);
+
+console.log(g/d);
+console.log(h/b);
+
+console.log(i++);
+console.log(c++);
+
+console.log(a--);
+console.log(b--);
